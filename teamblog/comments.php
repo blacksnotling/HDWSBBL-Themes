86b86e37d8539f044ead4cd443aa5575d7023512
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2017-12-09 17:51:50 +0000

    Reimplemented Comments
    Updated templates and style sheets to meet current standards

--- teamblog/comments.php
+++ teamblog/comments.php
@@ -1,109 +1,76 @@
-<?php // Do not delete these lines
-//V2. based of the new WP comments template from 2.7 and from http://themeshaper.com/wordpress-theme-comments-template-tutorial/
-// Do not delete these lines
-	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-		die ('Please do not load this page directly. Thanks!');
-
-	if ( post_password_required() ) { ?>
-		<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
-	<?php
-		return;
-	}
-?>
-
-<!-- You can start editing here. -->
-<div id="comments-list">
-
-<?php if ( have_comments() ) : ?>
-	<h3 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses' );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
-
-<?php /* If there are enough comments, build the comment navigation  */ ?>
-<?php $total_pages = get_comment_pages_count(); if ( $total_pages > 1 ) : ?>
-                                        <div id="comments-nav-above" class="comments-navigation">
-                                                                <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
-                                        </div><!-- #comments-nav-above -->
-<?php endif; ?>
-
-<?php /* An ordered list of our custom comments callback, custom_comments(), in functions.php   */ ?>
-			<ol>
-<?php wp_list_comments('type=comment&callback=crownstar_comment'); ?>
-			</ol>
-
-<?php /* If there are enough comments, build the comment navigation */ ?>
-<?php $total_pages = get_comment_pages_count(); if ( $total_pages > 1 ) : ?>
-                                <div id="comments-nav-below" class="comments-navigation">
-                                                <div class="paginated-comments-links"><?php paginate_comments_links(); ?></div>
-                </div><!-- #comments-nav-below -->
-<?php endif; ?>
-
- <?php else : // this is displayed if there are no comments so far ?>
-
-	<?php if ( comments_open() ) : ?>
-		<!-- If comments are open, but there are no comments. -->
-
-	 <?php else : // comments are closed ?>
-		<!-- If comments are closed. -->
-		<p class="nocomments">Comments are closed.</p>
-
-	<?php endif; ?>
-<?php endif; ?>
-
-</div><!-- end of .comments-list -->
-
-<?php if ( comments_open() ) : ?>
-
-<div id="respond">
-
-<h3 id="respond">Make your opinion known:</h3>
-<div class="info">
-	<p>Please note that the site reserves the right to edit or remove any comment without prior notice.</p>
-</div>
-
-<div class="cancel-comment-reply">
-	<small><?php cancel_comment_reply_link(); ?></small>
-</div>
-
-<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-<p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
-<?php else : ?>
-
-<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-
-<?php if ( is_user_logged_in() ) : ?>
-
-<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo wp_logout_url(get_permalink()); ?>" title="Log out of this account">Log out &raquo;</a></p>
-
-<?php else : ?>
-
-<p><input type="text" name="author" id="author" value="<?php echo esc_attr($comment_author); ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="author">Your Name <?php if ($req) echo "(required)"; ?></label></p>
-
-<p><input type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
-<label for="email">e-Mail (will not be published) <?php if ($req) echo "(required)"; ?></label></p>
-
-<p><input type="text" name="url" id="url" value="<?php echo esc_attr($comment_author_url); ?>" size="22" tabindex="3" />
-<label for="url">Your Website</label></p>
-
-<?php endif; ?>
-
-<!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
-
-<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
-
-<p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
-<?php comment_id_fields(); ?>
-</p>
-<?php do_action('comment_form', $post->ID); ?>
 <?php
-	if (function_exists( 'show_subscription_checkbox' )) {
-	//if the subscrive-to-comments plugin is active, have the option available
-		show_subscription_checkbox();
-	}
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
 ?>
 
-</form>
+<div id="comments" class="comments-area">
 
-<?php endif; // If registration required and not logged in ?>
-</div>
+	<?php
+	// You can start editing here -- including this comment!
+	if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'crownstar' ), get_the_title() );
+			} else {
+				printf(
+					/* translators: 1: number of comments, 2: post title */
+					_nx(
+						'%1$s Reply to &ldquo;%2$s&rdquo;',
+						'%1$s Replies to &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'crownstar'
+					),
+					number_format_i18n( $comments_number ),
+					get_the_title()
+				);
+			}
+			?>
+		</h2>
 
-<?php endif; // if you delete this the sky will fall on your head ?>
+		<ol class="comment-list">
+			<?php
+				wp_list_comments( array(
+					'avatar_size' => 40,
+					'style'       => 'ol',
+					'short_ping'  => true,
+					'reply_text'  => __( 'Reply', 'crownstar' ),
+				) );
+			?>
+		</ol>
+
+		<?php the_comments_pagination( array(
+			'prev_text' => '<span class="screen-reader-text">' . __( 'Previous', 'crownstar' ) . '</span>',
+			'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'crownstar' ) . '</span>',
+		) );
+
+	endif; // Check for have_comments().
+
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'crownstar' ); ?></p>
+	<?php
+	endif;
+
+	comment_form();
+	?>
+
+</div><!-- #comments -->
