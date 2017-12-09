3a71b983d8114d5dea2cc677045f95e1919462d7
blacksnotling <15323899+blacksnotling@users.noreply.github.com>
2017-12-09 21:49:18 +0000

    Corrected nested DIVs
    that were causing the page to look incorrect

--- oberwald/sidebar-content.php
+++ oberwald/sidebar-content.php
@@ -1,5 +1,5 @@
 <?php /* Dynamic content that goes at the bottom of the #maincontent section above the Footer */ ?>
 <div id="content-bottom">
 <?php if ( dynamic_sidebar('maincontent-bottom') ) : ?>
-	</div><!-- end of #content-bottom -->
 <?php endif; ?>
+</div><!-- end of #content-bottom -->
